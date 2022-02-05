6466ab15ef17522a4f93937f47febef4aec42758
mavluda27 <mavluda97@gmail.com>
2022-02-05 21:10:35 +0500

    upload files

---
+++ source/script.js
@@ -0,0 +1,33 @@
+function onClick() {
+  if (document.getElementById("participantGuest").checked) {
+    document
+      .getElementById("participantGuestLabel")
+      .classList.add("active-class");
+
+    document.getElementById("gradient-user-icon").classList.add("active-icon");
+    document.getElementById("white-user-icon").classList.remove("active-icon");
+    document.getElementById("white-user-icon").classList.add("no-active-icon");
+    document
+      .getElementById("participantSpeakerLabel")
+      .classList.remove("active-class");
+    document
+      .getElementById("gradient-micro-icon")
+      .classList.remove("active-icon");
+    document.getElementById("white-micro-icon").classList.add("active-icon");
+  } else if (document.getElementById("participantSpeaker").checked) {
+    document
+      .getElementById("participantSpeakerLabel")
+      .classList.add("active-class");
+
+    document.getElementById("gradient-micro-icon").classList.add("active-icon");
+    document.getElementById("white-micro-icon").classList.remove("active-icon");
+    document.getElementById("white-micro-icon").classList.add("no-active-icon");
+    document
+      .getElementById("gradient-user-icon")
+      .classList.remove("active-icon");
+    document.getElementById("white-user-icon").classList.add("active-icon");
+    document
+      .getElementById("participantGuestLabel")
+      .classList.remove("active-class");
+  }
+}
