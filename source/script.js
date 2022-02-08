87425ff60c73bdcb941c0896e8914cdf80e5d2e4
mavluda27 <mavluda97@gmail.com>
2022-02-08 09:14:04 +0500

    add hidden textarea

--- source/script.js
+++ source/script.js
@@ -1,6 +1,9 @@
 function onClick() {
   if (document.getElementById("participantGuest").checked) {
     document
+      .getElementById("hidden-textarea")
+      .classList.remove("show-hidden-textarea");
+    document
       .getElementById("participantGuestLabel")
       .classList.add("active-class");
 
@@ -15,6 +18,9 @@ function onClick() {
       .classList.remove("active-icon");
     document.getElementById("white-micro-icon").classList.add("active-icon");
   } else if (document.getElementById("participantSpeaker").checked) {
+    document
+      .getElementById("hidden-textarea")
+      .classList.add("show-hidden-textarea");
     document
       .getElementById("participantSpeakerLabel")
       .classList.add("active-class");
